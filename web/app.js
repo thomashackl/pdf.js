f11f67aa1d9f2f8060250d6d20347a36ff39c0de
Calixte Denizet <calixte.denizet@gmail.com>
2025-05-07 19:07:21 +0200

    Add a pref in order to cap the canvas area to a factor of the window one (bug 1958015)
    This way it helps to reduce the overall canvas dimensions and make the rendering faster.
    The drawback is that when scrolling, the page can be blurry in waiting for the rendering.
    
    The default value is 200% on desktop and will be 100% for GeckoView.

--- web/app.js
+++ web/app.js
@@ -362,6 +362,7 @@ const PDFViewerApplication = {
     // Set some specific preferences for tests.
     if (typeof PDFJSDev !== "undefined" && PDFJSDev.test("TESTING")) {
       Object.assign(opts, {
+        capCanvasAreaFactor: x => parseInt(x),
         docBaseUrl: x => x,
         enableAltText: x => x === "true",
         enableAutoLinking: x => x === "true",
@@ -495,7 +496,8 @@ const PDFViewerApplication = {
 
     const enableHWA = AppOptions.get("enableHWA"),
       maxCanvasPixels = AppOptions.get("maxCanvasPixels"),
-      maxCanvasDim = AppOptions.get("maxCanvasDim");
+      maxCanvasDim = AppOptions.get("maxCanvasDim"),
+      capCanvasAreaFactor = AppOptions.get("capCanvasAreaFactor");
     const pdfViewer = (this.pdfViewer = new PDFViewer({
       container,
       viewer,
@@ -526,6 +528,7 @@ const PDFViewerApplication = {
       enablePrintAutoRotate: AppOptions.get("enablePrintAutoRotate"),
       maxCanvasPixels,
       maxCanvasDim,
+      capCanvasAreaFactor,
       enableDetailCanvas: AppOptions.get("enableDetailCanvas"),
       enablePermissions: AppOptions.get("enablePermissions"),
       pageColors,
